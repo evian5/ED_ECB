aea3f430056cc173ba551a82480fb7aa6a6730ed
evian <evian@gmail.com>
2025-04-30 18:42:51 +0200

    ECB_Commit de la primera clase de persona

--- Persona_ECB.java
+++ Persona_ECB.java
@@ -6,5 +6,59 @@ class Persona_ECB {
         private double masa;
         private double altura;
         private int edad;
+
+        @Override
+        public String toString() {
+            return "Persona [id=" + id + ", nombre=" + nombre + ", masa=" + masa + ", altura=" + altura + ", edad="
+                    + edad + "]";
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(int nombre) {
+            this.nombre = nombre;
+        }
+
+        public double getMasa() {
+            return masa;
+        }
+
+        public void setMasa(double masa) {
+            this.masa = masa;
+        }
+
+        public double getAltura() {
+            return altura;
+        }
+
+        public void setAltura(double altura) {
+            this.altura = altura;
+        }
+
+        public int getEdad() {
+            return edad;
+        }
+
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+
+        public Persona(int id, int nombre, double masa, double altura, int edad) {
+            this.id = id;
+            this.nombre = nombre;
+            this.masa = masa;
+            this.altura = altura;
+            this.edad = edad;
+        }
     }
 }
