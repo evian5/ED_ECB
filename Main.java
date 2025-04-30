14ba53e1c1a7fb28cdf9016dd0327744b05cac0b
evian <evian@gmail.com>
2025-04-30 19:50:25 +0200

    ECB_Inserccion metodos en clase

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+class main {
+    public class Main {
+        Persona pers1 = new Persona(1, " Papa", 85, 1.75, 22);
+        Persona pers2 = new Persona(2, " Papa", 75, 1.80, 25);
+
+        GravedadPlanetaria plan1 = new GravedadPlaneta(1, "Tierra", 9.81);
+        GravedadPlanetaria plan2 = new GravedadPlaneta(2, "Marte", 3.72);
+        GravedadPlanetaria plan3 = new GravedadPlaneta(3, "Jupiter", 24.79);
+
+    }
+}
