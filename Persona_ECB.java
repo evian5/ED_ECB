d091bb65814bf3529bf94ee7036dd5ea8313223a
evian <evian@gmail.com>
2025-04-30 18:40:30 +0200

    ECB_Commit de la primera clase de persona

---
+++ Persona_ECB.java
@@ -0,0 +1,10 @@
+class Persona_ECB {
+    public class Persona {
+
+        private int id;
+        private int nombre;
+        private double masa;
+        private double altura;
+        private int edad;
+    }
+}
